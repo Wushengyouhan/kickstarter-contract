c19062310fec551f6c59483dbb37072cf94660c0
Wushengyouhan <840835067@qq.com>
2025-03-05 11:48:29 +0800

    167. CampaignFactory Instance

---
+++ ethereum/factory.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+import CampaignFactory from './build/CampaignFactory.json';
+
+const instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  ' 0x229e4ea6675e43E0eC684b31372511d724873a92'
+);
+
+export default instance;
