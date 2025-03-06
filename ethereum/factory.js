7b9c4712cdd2fdf0cacc7e590a3bf5e330e10727
Wushengyouhan <840835067@qq.com>
2025-03-06 10:04:07 +0800

    172. Server vs Client Web3 Instances

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  ' 0x229e4ea6675e43E0eC684b31372511d724873a92'
+  '0x229e4ea6675e43E0eC684b31372511d724873a92'
 );
 
 export default instance;
