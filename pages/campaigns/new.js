32a5c68b015dafef372dabb5b073b0981c28b6ac
Wushengyouhan <840835067@qq.com>
2025-03-11 10:37:32 +0800

    190. Button Spinners

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -8,11 +8,12 @@ class CampaignNew extends Component {
   state = {
     minimumContribution: "",
     errorMessage: "",
+    loading: false,
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
-
+    this.setState({ loading: true, errorMessage: "" });
     try {
       const accounts = await web3.eth.getAccounts();
       await factory.methods
@@ -21,6 +22,7 @@ class CampaignNew extends Component {
     } catch (err) {
       this.setState({ errorMessage: err.message });
     }
+    this.setState({ loading: false });
   };
 
   render() {
@@ -40,7 +42,9 @@ class CampaignNew extends Component {
             />
           </Form.Field>
           <Message error header="Opps!" content={this.state.errorMessage} />
-          <Button primary>Create!</Button>
+          <Button primary loading={this.state.loading}>
+            Create!
+          </Button>
         </Form>
       </Layout>
     );
