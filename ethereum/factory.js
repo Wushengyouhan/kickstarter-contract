d18b9e4300d31bcd748df9949f16e49f9730de87
Wushengyouhan <840835067@qq.com>
2025-03-12 10:00:15 +0800

    201. CampaignShow's GetInitialProps

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,9 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x229e4ea6675e43E0eC684b31372511d724873a92'
+  "0x70E262d594746C9907180A74e203F45a585a0E83"
 );
 
 export default instance;
