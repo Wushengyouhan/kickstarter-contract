2e51fe8bce9cf5ccf29c58536c5889c3557d8051
Wushengyouhan <840835067@qq.com>
2025-03-12 10:53:41 +0800

    204. Custom Card Groups

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,24 +1,49 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
+import { Card } from "semantic-ui-react";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
     const campaign = Campaign(props.query.address);
     const summary = await campaign.methods.getSummary().call();
+
     return {
-      minimumContribution: summary[0],
-      balance: summary[1],
-      requestsCount: summary[2],
-      approversCount: summary[3],
+      minimumContribution: summary[0].toString(),
+      balance: summary[1].toString(),
+      requestsCount: summary[2].toString(),
+      approversCount: summary[3].toString(),
       manager: summary[4],
     };
+  }
+
+  renderCards() {
+    const {
+      minimumContribution,
+      balance,
+      requestsCount,
+      approversCount,
+      manager,
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: "Address of Manager",
+        description:
+          "The manager created this campaign and can create requests to withdraw money",
+        style: { overflowWrap: "break-word" },
+      },
+    ];
+
+    return <Card.Group items={items} />;
   }
 
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
